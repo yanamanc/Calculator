071c3fe150bc1c079623ef88f97f2ec443a436dc
Yana Mancevich <yaninamantsevich@mail.ru>
2018-09-11 12:21:17 +0200

    task

---
+++ app/controllers/calculators_controller.rb
@@ -0,0 +1,10 @@
+class CalculatorsController < ApplicationController
+      def index
+  end
+
+  def new
+    @result = (params[:a].to_i - params[:b].to_i) * (100 + params[:c].to_i) * 0.01 / params[:d].to_i
+    render :index
+  end
+
+end
